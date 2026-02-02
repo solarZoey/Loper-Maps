1f65af0ac088c6fab0f9ee99511c30647bd8842e
cooliestcoop <wittec@lopers.unk.edu>
2026-02-02 11:59:46 -0600

    More A*
    Added more of the starter code. Started development of the A* algorithm. Will continue at a later date.

--- Code Stuff/A_Star.py
+++ Code Stuff/A_Star.py
@@ -49,3 +49,59 @@ class Edge:
             else:
                 return self.connections[0]
             
+# Class represneting the graph data structure
+class Graph:
+    def __init__(self):
+        self._nodes = []
+        self._edges = []
+
+    # Adds node to graph by adding it to end of node list
+    def addNode(self, _n: Node):
+        self._nodes.append(_n)
+
+    # Connects two nodes by creating an edge and assigning it to the two parameter nodes
+    def addConnection(self, _n1: Node, _n2: Node, cost: int):
+        e = Edge(cost)
+        e.connectNodes(_n1, _n2)
+        self._edges.append(e)
+
+    # Calculates heuristic using Euclidean Distance
+    def calcHeuristic(self, _n1: Node, _n2: Node) -> float:
+        return math.sqrt((_n1.latitude - _n2.latitude) ** 2 + (_n1.longitude -_n2.longitude) ** 2)
+    
+    # A* Algorithm
+    def a_star(self, start: Node, goal: Node):
+        start.g = 0
+        start.h = self.calcHeuristic(start, goal)
+        start.f = start.g + start.h
+
+        queue = [start]
+        processed = []
+
+        while queue:
+            current = queue.pop(0)
+            print(f"\nVisiting {current.name}")
+            print(f"G Cost: {current.g}")
+            print(f"Heuristic Value: {current.h:.2f}")
+            print(f"F Cost: {current.f:.2f}")
+            processed.append(current)
+
+            if current == goal:
+                print("\nFOUND GOAL\n")
+                path = [] # Initializes list for the path
+                total_cost = current.g # Total cost of path
+                
+                while current:
+                    print(f"Adding {current.name} to path")
+                    path.append(current.name)
+                    current = current.getBacktrack() # Sets current to previous node from the goal
+                    print(f"\nTotal Cost: {total_cost}")
+                    return path[::-1] # Returns reversed path for correct order of nodes
+                
+            for edge in current.edges:
+                neighbor = edge.getOther(current)
+                print(f"\nFound neighbor {neighbor.name}")
+                tentative_g = current.g + edge.weight # Calculates tentative cost to reach neighbor from initial node
+
+                if tentative_g < neighbor.g: # If new path is better
+                    print(f"Updating G Cost: ")
