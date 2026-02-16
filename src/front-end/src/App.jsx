d99e3fcf0852893ff3d65ede5b415ec28070a3d6
zzzoey <solarzn@lopers.unk.edu>
2026-02-16 14:32:46 -0600

    init front end non-functional scaffolding for react+vite moved images out of `front-end/` so they wouldn't get deleted by the build process

---
+++ src/front-end/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>test page :3</p>
+    </>
+  )
+}
+
+export default App
