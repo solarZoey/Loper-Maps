5c6e0aebc0d7c3524708185c4d2d21bf3369d536
SloanG1 <147006828+SloanG1@users.noreply.github.com>
2026-03-25 11:35:43 -0500

    changes to live_tracking

--- src/back-end/Live_Tracking.py
+++ src/back-end/Live_Tracking.py
@@ -4,12 +4,33 @@ import time
 
 class Live_Tracking:
     # data attributes
+    __live_lat = -1
+    __live_long = -1
+    __wait_time = -1
+
+    # init
 
     # helpers
 
     # getters
+    def get_live_lat(self):
+        return self.__live_lat
+
+    def get_live_long(self):
+        return self.__live_long
+
+    def get_wait_time(self):
+        return self.__wait_time
 
     # setters
+    def set_live_lat(self, live_lat):
+        self.__live_lat = live_lat
+
+    def set_live_long(self, live_long):
+        self.__live_long = live_long
+
+    def set_wait_time(self, wait_time):
+        self.__wait_time = wait_time
 
     # tostring
 
